33b0fc51171f18d8188a0c4b0f8104360c28deec
Jindouz <141655429+Jindouz@users.noreply.github.com>
2024-02-12 04:29:35 -0800

    Add files via upload

---
+++ base/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+# login
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+        token['superuser'] = user.is_superuser
+        # ...
+        return token
+
+
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+# register
+@api_view(['POST'])
+def register(request):
+    user = User.objects.create_user(
+                username=request.data['username'],
+                email=request.data['email'],
+                password=request.data['password']
+            )
+    user.is_active = True
+    user.is_staff = True
+    user.save()
+    return Response("new user born")
+
+
+@api_view(['GET'])
+def test(request):
+    return Response({'test':'done'})
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def test_auth(request):
+    # get the logged user..
+    print( request.user)
+    return Response({'connected user':str( request.user)})
